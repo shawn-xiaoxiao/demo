1d0fcbdb0c74919d2c836100cd35ab455e5e9d7c
shawn-show <2079947408@qq.com>
2016-11-01 23:06:08 +0800

    first test

--- src/main/java/com/demo/MainSetup.java
+++ src/main/java/com/demo/MainSetup.java
@@ -1,15 +1,14 @@
 package com.demo;
 
+import org.nutz.ioc.Ioc;
 import org.nutz.mvc.NutConfig;
 import org.nutz.mvc.Setup;
 
-/**
- * Created by Administrator on 2016/11/1 0001.
- */
+
 public class MainSetup implements Setup {
 
     public void init(NutConfig nutConfig) {
-
+        Ioc ioc = nutConfig.getIoc();
     }
 
     public void destroy(NutConfig nutConfig) {
